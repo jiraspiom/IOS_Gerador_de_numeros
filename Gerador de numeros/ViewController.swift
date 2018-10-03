6956320d9b56c3b72c2d481a4e31465e14592022
gilmar <jiraspiom@yahoo.com.br>
2018-10-03 14:15:59 -0300

    finalizado

--- Gerador de numeros/ViewController.swift
+++ Gerador de numeros/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var legendaResultado: UILabel!
+   
+    @IBAction func gerarResultado(_ sender: Any) {
+        
+        let numero = arc4random_uniform(11)
+        
+        legendaResultado.text = String(numero)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
